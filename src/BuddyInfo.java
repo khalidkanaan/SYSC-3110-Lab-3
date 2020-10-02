b3377ba90252c28e355caa1829b50436b83d0071
Khalid <khalidkanaan@cmail.carleton.ca>
2020-10-02 15:29:47 -0400

    BuddyInfo Commit File

---
+++ src/BuddyInfo.java
@@ -0,0 +1,20 @@
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private int phoneNumber;
+    public BuddyInfo(String name, String address, int phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddyinfo = new BuddyInfo("Tom", "home", 1234);
+        System.out.println("Hello "+ buddyinfo.getName());
+    }
+
+}
